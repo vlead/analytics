ffb0c4b5ed2a6d90857fa692c757407ef37b108c
madhavi <madhavi@localhost.localdomain>
2015-06-30 15:43:03 +0530

    implemented cache code

--- src/app.py
+++ src/app.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, request, jsonify, make_response
+# import the flask extension
+from flask.ext.cache import Cache
 import json
 app = Flask(__name__)
 
+# define the cache config keys, remember that it can be done in a settings file
+app.config['CACHE_TYPE'] = 'simple'
+
+# register the cache instance and binds it on to your app 
+app.cache = Cache(app)
+
 @app.route('/numberofhits')
+@app.cache.cached(timeout=100)  # cache this view for 5 minutes
+
 def numberofhits():
     json_data = open("data-2014.json")
     data = json.load(json_data)
@@ -14,6 +24,4 @@ def numberofhits():
     #return jsonify(data=json_string)
 
 if __name__ == '__main__':
-    app.run(
-        debug=True
-    )
+    app.run(port=5000,debug=True)
